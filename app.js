ef4c29ca625112e2db7c4c7e4edde3db2f6d64d7
yael <yaelu@example.com>
2025-11-16 21:48:06 +0200

    morgan and cors

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ import express from 'express'
 import {DayMiddleware} from './middlewares/currentDate.middlewares.js';
 import {msgGet} from './middlewares/DateGet.middlewares.js';
 import {error} from './middlewares/error.middlewares.js';
+import cors from 'cors';
+import morgan from 'morgan';
 const app=express()
+//fetch('http://localhost:3000/book')
+app.use(morgan('dev'));
+app.use(cors({origin:'http://localhost:8080'}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(DayMiddleware);
